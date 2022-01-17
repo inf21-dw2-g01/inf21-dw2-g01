24d74fb29772e2e854c0d8090d34fceb1e23c1d9
unknown <mariofgrodrigues2@gmail.com>
2022-01-17 23:34:01 +0000

    Sexto Commit

--- DW2/BD/database1.sql
+++ DW2/BD/database1.sql
@@ -10,14 +10,19 @@ CREATE TABLE `Filmes`(
     `release_date` date NOT NULL,
     `runtime` int NOT NULL,
     `genre` nvarchar(50) NOT NULL,
+    `generoID` int NOT NULL,
     PRIMARY KEY (`id`)
+    FOREIGN KEY (generoId) REFERENCES genero(id)
+    FOREIGN KEY (listaId) REFERENCES lista_filmes(id_filme)
 )ENGINE=InnoDB;
 
 
 CREATE TABLE `genero`(
     `id` bigint NOT NULL AUTO_INCREMENT,
     `genero` nvarchar(50) NOT NULL,
+    `filmesID` int NOT NULL,
     PRIMARY KEY (`id`)
+    FOREIGN KEY (filmesId) REFERENCES Filmes(id)
 )ENGINE=InnoDB;
 
 CREATE TABLE `genero_filme`(
@@ -32,14 +37,20 @@ CREATE TABLE `user`(
     `last_name` nvarchar(20) NOT NULL,
     `mail` nvarchar(50) NOT NULL, 
     `favoritos` int,
+    `listaID` int NOT NULL,
     PRIMARY KEY (`id`)
+    FOREIGN KEY (listaId) REFERENCES lista_filme(id_lista)
 )ENGINE=InnoDB;
 
 
 CREATE TABLE `lista_filme`(
     `id` bigint NOT NULL,
     `id_lista` int NOT NULL,
-    `id_filme` int NOT NULL
+    `id_filme` int NOT NULL,
+    `userID` int NOT NULL,
+    `filmesID` int NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(id)
+    FOREIGN KEY (filmesId) REFERENCES Filmes(id)
 )ENGINE=InnoDB;
 
 
